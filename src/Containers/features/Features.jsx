810670c677177c9b071032156450e40c7054f722
jdharr22 <harris.jalen@gmail.com>
2024-01-11 15:51:49 -0600

    Update

--- src/Containers/features/Features.jsx
+++ src/Containers/features/Features.jsx
@@ -5,6 +5,7 @@ import "./features.css";
 
 import app from "../../assets/app.png";
 import calendar from "../../assets/calendar.png";
+import dashboard from "../../assets/dashboard.png";
 
 const Links = () => (
   <>
@@ -26,42 +27,58 @@ const Links = () => (
 const Features = () => {
   return (
     <div className="elitefit_features section__padding" id="features">
-      <div className="elitefit__features-content">
+      <div className="elitefit__features-heading">
         <h1 className="gradient__text">Features </h1>
         <p>Key features include...</p>
-        <div className="elitefit__features-content-cards1">
-          <div className="elitefit__features-content-cards1_app">
-            <div className="card">
-              <div className="card_img">
-                <img src={app} alt="app"></img>
-              </div>
-              <div className="card__header">
-                <p>
-                  <a href="#app">Client/Coach App</a>
-                </p>
-              </div>
-              <div className="card__text">
-                <p>
-                  Some quick example text to build on the card title and make up
-                  the bulk of the card's content.
-                </p>
-              </div>
+      </div>
+      <div className="elitefit__features-cards section__padding">
+        <div className="elitefit__features-cards_app1">
+          <div className="card">
+            <div className="card_img">
+              <img src={app} alt="app"></img>
             </div>
-            <div className="card">
-              <div className="card_img">
-                <img src={calendar} alt="app"></img>
-              </div>
-              <div className="card__header">
-                <p>
-                  <a href="#app">Client/Coach App</a>
-                </p>
-              </div>
-              <div className="card__text">
-                <p>
-                  Some quick example text to build on the card title and make up
-                  the bulk of the card's content.
-                </p>
-              </div>
+            <div className="card__header">
+              <p>
+                <a href="#app">Client/Coach App</a>
+              </p>
+            </div>
+            <div className="card__text">
+              <p>
+                Some quick example text to build on the card title and make up
+                the bulk of the card's content.
+              </p>
+            </div>
+          </div>
+          <div className="card">
+            <div className="card_img">
+              <img src={calendar} alt="app"></img>
+            </div>
+            <div className="card__header">
+              <p>
+                <a href="#app">Client/Coach App</a>
+              </p>
+            </div>
+            <div className="card__text">
+              <p>
+                Some quick example text to build on the card title and make up
+                the bulk of the card's content.
+              </p>
+            </div>
+          </div>
+          <div className="card">
+            <div className="card_img">
+              <img src={dashboard} alt="app"></img>
+            </div>
+            <div className="card__header">
+              <p>
+                <a href="#dashboard">Dashboard</a>
+              </p>
+            </div>
+            <div className="card__text">
+              <p>
+                Some quick example text to build on the card title and make up
+                the bulk of the card's content.
+              </p>
             </div>
           </div>
         </div>
